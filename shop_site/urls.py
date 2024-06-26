1bdcb87f5a8d1c57d488596b6a6735ed75b52be6
jhin-1 <ahmedyosri52@gmail.com>
2024-06-26 09:25:58 +0300

    change endpoint

--- shop_site/urls.py
+++ shop_site/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from shop_site import views
 urlpatterns = [
-    path('home', views.index, name='home'),
+    path('', views.index, name='home'),
     path('details/<int:id>', views.details, name='details'),
     path('shop', views.shop, name='shop'),
     path('cart/', views.all_cart, name='cart'),
--- shopsite/urls.py
+++ shopsite/urls.py
@@ -18,8 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from shop_site import urls as app_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('shop_site.urls'))
+    path('', include(app_urls))
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
